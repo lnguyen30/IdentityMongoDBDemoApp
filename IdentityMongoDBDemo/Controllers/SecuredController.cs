6f074196ed0346a5c2f47cf47db7a7d65d4e22ea
lnguyen30 <lnguyen30@uco.edu>
2021-09-12 22:31:21 -0500

    Add project files.

---
+++ IdentityMongoDBDemo/Controllers/SecuredController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityMongoDBDemo.Controllers
+    
+{
+    public class SecuredController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ IdentityMongoDBDemo/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityMongoDBDemo.Models
+{
+    public class User
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+
+
+    }
+}
